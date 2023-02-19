ce0059543a4a3564a1055b6cca61d869c433fcbf
Aris Ripandi <riipandi@gmail.com>
2023-02-20 03:12:27 +0700

    change test scenario

--- test_script.js
+++ test_script.js
@@ -10,19 +10,21 @@ import http from 'k6/http'
  * Load test user authentication using k6:
  * Docs: https://k6.io/docs/get-started/running-k6
  *
- *  k6 run --vus 1000 --iterations 1000 test_script.js
+ *  k6 run test_script.js --vus 200 --iterations 5000 --duration 1m --no-summary
+ *  k6 run test_script.js --vus 200 --duration 1m --no-thresholds --no-summary
  */
 export default function () {
-  const url = 'http://localhost:9999/api/v1/login'
-  const payload = JSON.stringify({
-    email: 'admin@example.com',
-    password: 'passw0rd',
-  })
-  const params = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }
-  http.post(url, payload, params)
+  // const url = 'http://127.0.0.1:9999/api/v1/login'
+  // const payload = JSON.stringify({
+  //   email: 'admin@example.com',
+  //   password: 'passw0rd',
+  // })
+  // const params = {
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  // }
+  // http.post(url, payload, params)
+  http.get('http://127.0.0.1:9999/api/user')
   sleep(1)
 }
